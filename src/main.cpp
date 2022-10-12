7473bae61846668a8e6a7e27689586537bd38643
Matyas <35014820+MatyasEngelen@users.noreply.github.com>
2022-10-12 12:36:00 -0700

    Added cps select

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,22 @@
 #define NOMINMAX
 #include <Windows.h>
 
+int cps;
+int timeOut;
 
 int main()
 {
+    printf("Enter CPS you lazy mf: ");
+    std::cin >> cps;
+
+    timeOut = 1000 / cps;
+
     for (;;) {
         if (GetAsyncKeyState(VK_F1))
         {
             mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, NULL, NULL);
             mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, NULL, NULL);
-            printf("click");
-            Sleep(33);
+            Sleep(timeOut);
         }
     }
 }
